0ee0511d53c0dbed7f738d81cb13a866103855a1
Franco Ghilardi <franco.ghilardi1@gmail.com>
2024-11-24 11:47:20 -0300

    Se agregan body y footer

---
+++ src/components/organisms/Footer/styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'blue',
+    marginTop: '5%',
+    height: '10%',
+  },
+});
